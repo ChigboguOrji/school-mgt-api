fa5e8a6ee8af1820e050340537a1fcf68682d5cd
Chigbogu Orji <brightorji60@gmail.com>
2021-01-04 17:22:55 +0100

    Modify schema

--- src/scores/model.js
+++ src/scores/model.js
@@ -2,26 +2,24 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const grades = 'excellent credit distinction pass good fail'.split(' ')
 
-const ScoreSchema = new Schema({
-  student:   {type: Schema.Types.Mixed, required: true},
-  classId:   String,
-  sessionId: String,
-  termId:    String,
-  marks:     [
-    {
-      subject: {type: String, lowercase: true},
-      r_test:  Number,
-      c_asses: Number,
-      test:    Number,
-      exams:   Number,
-      total:   Number,
-      grade:   {
-        type:      String,
-        lowercase: true,
-        enum:      grades
-      }
-    }
-  ]
+/**
+ * @scores
+ * @param {number} r_test resumption test
+ * @param {number} c_asses continuous assessment
+ * @param {number} test termly test
+ * @param {number} exam exams score
+ * @param {number} total total score in a subject
+ */
+
+const Result = new Schema({
+  student:    {type: Schema.Types.ObjectId, ref: 'Student'},
+  class_id:   String,
+  session_id: String,
+  term_id:    String,
+  scores:     [{subject: String, r_test: Number, c_asses: Number, test: Number, 
+    exams: Number, total: Number}],
+  grade:      {type: String, lowercase: true, enum: grades},
+  average:    {type: Number, default: 0}
 })
 
-module.exports = mongoose.model('Score', ScoreSchema, 'scores')
+module.exports = mongoose.model('Result', Result)
